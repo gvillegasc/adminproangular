893270f65e7a3410eb2a0e2f683d7f3a10fcb13d
gvillegasc <villegasgerardo.j@gmail.com>
2020-01-12 23:22:29 -0500

    Agregar y modificar modulos

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,27 @@
+// Encargado para menjar las rutas
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { PagesComponent } from "./pages.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ProgressComponent } from "./progress/progress.component";
+import { Graficas1Component } from "./graficas1/graficas1.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: PagesComponent,
+    children: [
+      { path: "dashboard", component: DashboardComponent },
+      { path: "progress", component: ProgressComponent },
+      { path: "graficas1", component: Graficas1Component },
+      { path: "", pathMatch: "full", redirectTo: "/dashboard" }
+    ]
+  }
+];
+export const PagesRoutingModule = RouterModule.forChild(routes);
+// @NgModule({
+// 	imports: [RouterModule.forChild(routes)],
+// 	exports: [RouterModule]
+// })
+// export class PagesRoutingModule {}
